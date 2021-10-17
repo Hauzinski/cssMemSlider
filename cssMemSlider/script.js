b9310ef17bf3157bf8771bc8eb09d0e43261b3f2
Vitali Hauzinski <hauzinski@gmail.com>
2021-10-17 12:37:04 +0300

    feat: add text change in slider (Sun, Oct 17, 2021 12:37:04 PM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -2,18 +2,17 @@ const controls = document.getElementById("controls");
 const dots = document.querySelectorAll(".controls__dot");
 const sld = document.querySelector(".slider");
 const sldImages = document.querySelector(".slider__images");
+const text = document.querySelectorAll(".text");
 
 let slideNow = 0;
 
 function dotCheck(event) {
-  
-
   if (event.target.className === "controls__dot") {
     for (let value of dots) {
       value.classList.remove("controls__dot_selected");
-    }
+    }    
     event.target.classList.add("controls__dot_selected");
-    slideNow = +event.target.id[4] - 1;
+    slideNow = +event.target.id[4] - 1;    
   } else if (event.target.className === "controls__dot-container") {
     for (let value of dots) {
       value.classList.remove("controls__dot_selected");
@@ -24,7 +23,12 @@ function dotCheck(event) {
     return;
   }
 
-  sldImages.style.transform = `translateX(-${sld.clientWidth * slideNow}px)`;  
+  for (let value of text) {
+    value.hidden = true;
+  }
+  document.getElementById(`text-${slideNow + 1}`).hidden = false;
+  // document.getElementById(`text-${slideNow + 1}`).style.color = "red";
+  sldImages.style.transform = `translateX(-${sld.clientWidth * slideNow}px)`;
 }
 
 controls.addEventListener("mousedown", dotCheck);
