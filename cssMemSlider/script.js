28c0f1e95eae03547a0de45815078b689279e9cf
Vitali Hauzinski <hauzinski@gmail.com>
2021-10-17 11:49:42 +0300

    feat: add large control click area (Sun, Oct 17, 2021 11:49:42 AM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -6,15 +6,25 @@ const sldImages = document.querySelector(".slider__images");
 let slideNow = 0;
 
 function dotCheck(event) {
-  if (event.target.tagName === "SPAN") {
+  
+
+  if (event.target.className === "controls__dot") {
     for (let value of dots) {
       value.classList.remove("controls__dot_selected");
     }
-
     event.target.classList.add("controls__dot_selected");
     slideNow = +event.target.id[4] - 1;
+  } else if (event.target.className === "controls__dot-container") {
+    for (let value of dots) {
+      value.classList.remove("controls__dot_selected");
+    }
+    event.target.firstElementChild.classList.add("controls__dot_selected");
+    slideNow = +event.target.firstElementChild.id[4] - 1;
+  } else {
+    return;
   }
-  sldImages.style.transform = `translateX(-${sld.clientWidth * slideNow}px)`;
+
+  sldImages.style.transform = `translateX(-${sld.clientWidth * slideNow}px)`;  
 }
 
 controls.addEventListener("mousedown", dotCheck);
